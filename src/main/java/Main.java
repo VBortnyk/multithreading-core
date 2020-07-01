f9fb2f5ab807383618677a0048488126a838dd0f
VBortnyk <viacheslav.bortnyk@gmail.com>
2020-07-01 14:26:19 +0300

    refactored thread creation in main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Counter counter = new Counter(0, 50);
-        new Thread(new ThreadOne(counter)).start();
+        new ThreadOne(counter).start();
         new Thread(new ThreadTwo(counter)).start();
     }
 }
